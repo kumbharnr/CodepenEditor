c548956700b177273e238e25ab6f56878cc767a6
Navnath Kumbhar <kumbharnr777@gmail.com>
2021-12-28 10:13:51 +0530

    first commit

---
+++ Codepen/JavaScript/ind.js
@@ -0,0 +1,30 @@
+function run(){
+    let html = document.querySelector(".editor-type #html-code").value;
+    let css = "<style>" + document.querySelector(".editor-type #css-code").value + "</style>";
+    let js = document.querySelector(".editor-type #js-code").value;
+    let output = document.querySelector(".result #output");
+  
+    output.contentDocument.body.innerHTML  = html;
+    output.contentDocument.head.innerHTML  = css;
+    output.contentWindow.eval(js); 
+  }
+  document.querySelector(".editor-type #html-code").addEventListener('keyup', run);
+  document.querySelector(".editor-type #css-code").addEventListener('keyup', run);
+  document.querySelector(".editor-type #js-code").addEventListener('keyup', run);
+  
+  function first(){
+    let output = document.querySelector(".result #output");
+    output.contentDocument.body.style.backgroundColor = "white";
+  }
+  document.querySelector(".editor-type #html-code").addEventListener('keydown', first);
+  document.querySelector(".editor-type #css-code").addEventListener('keydown', first);
+  document.querySelector(".editor-type #js-code").addEventListener('keydown', first);
+
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
